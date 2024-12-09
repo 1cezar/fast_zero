6b2e314b38cc60d9774de56febd4e5919c0bf20a
1cezar <cezaugusto.rocha@gmail.com>
2024-12-09 11:21:48 -0300

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from fast_zero.app import app
+from http import HTTPStatus
+
+client = TestClient(app)
+
+def test_read_root():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "Hello World"}
